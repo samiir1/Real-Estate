e9e041cd77d9a4dbe4d05211cfb22ddcdbc39b7b
Farhan <samiir1435@gmail.com>
2023-10-21 16:44:02 +0300

    Creating Pages

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+import About from './pages/About'
+import Profile from './pages/Profile'
+
+export default function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/Login' element={<Login/>}/>
+      <Route path='/Logout' element={<Logout/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
