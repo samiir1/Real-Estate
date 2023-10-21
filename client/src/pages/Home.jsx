fe56f5f4a2bc4fa4f81948bf188359a9dd20873c
Farhan <samiir1435@gmail.com>
2023-10-21 16:58:31 +0300

    test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>This is a Home page for testing and Romouts</div>
+    <div>This is a Home page for testing and Romoutss</div>
   )
 }
 
