473ab0f16515209036d49bbb09205b43e4886d2b
Farhan <samiir1435@gmail.com>
2023-10-21 18:29:03 +0300

    Header Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import Login from './pages/Login'
 import Logout from './pages/Logout'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './componants/Header'
 
 export default function App(){
   return(
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/Login' element={<Login/>}/>
